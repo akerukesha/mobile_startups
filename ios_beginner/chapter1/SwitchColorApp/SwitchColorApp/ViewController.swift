eb8566ef390f3b51e8b82a89dde943fb58dd95f0
akerukesha <okapova.akerke@gmail.com>
2018-01-18 16:52:19 +0600

    Initial background color added

--- ios_beginner/chapter1/SwitchColorApp/SwitchColorApp/ViewController.swift
+++ ios_beginner/chapter1/SwitchColorApp/SwitchColorApp/ViewController.swift
@@ -26,14 +26,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        background.backgroundColor = getRandomColor()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
