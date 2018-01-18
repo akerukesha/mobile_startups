94216d58b746e5d41b3dc204eb4b44eac3431063
akerukesha <okapova.akerke@gmail.com>
2018-01-18 16:28:32 +0600

    First commit

---
+++ ios_beginner/chapter1/SwitchColorApp/SwitchColorApp/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  SwitchColorApp
+//
+//  Created by Akerke Okapova on 1/18/18.
+//  Copyright © 2018 Akerke Okapova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var background: UIView!
+    
+    @IBAction func switchColorButtonPressed(_ sender: Any) {
+        background.backgroundColor = getRandomColor()
+    }
+    
+    func getRandomColor() -> UIColor {
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
