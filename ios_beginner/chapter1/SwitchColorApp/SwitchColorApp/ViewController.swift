41771647b4baf052f50522977b41c7163b4fd2b1
akerukesha <okapova.akerke@gmail.com>
2018-01-23 12:15:45 +0600

    RGB shades added

--- ios_beginner/chapter1/SwitchColorApp/SwitchColorApp/ViewController.swift
+++ ios_beginner/chapter1/SwitchColorApp/SwitchColorApp/ViewController.swift
@@ -11,9 +11,39 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var background: UIView!
+    @IBOutlet weak var switchOrdinaryButton: UIButton!
     
     @IBAction func switchColorButtonPressed(_ sender: Any) {
-        background.backgroundColor = getRandomColor()
+        switchOrdinaryButton.backgroundColor = getRandomColor()
+    }
+    
+    @IBAction func switchRedColorButtonPressed(_ sender: Any) {
+        background.backgroundColor = getShade(color: "red")
+    }
+    
+    @IBAction func switchGreenColorButtonPressed(_ sender: Any) {
+        background.backgroundColor = getShade(color: "green")
+    }
+    
+    @IBAction func switchBlueColorButtonPressed(_ sender: Any) {
+        background.backgroundColor = getShade(color: "blue")
+    }
+    
+    private let colorRanges: [String: (Int, Int)] = [
+        "red": (0, 18),
+        "green": (63, 178),
+        "blue": (179, 257)
+    ]
+    
+    func getShade(color: String) -> UIColor {
+        let hue = CGFloat(Float(getRandomInt(mn: (colorRanges[color]?.0)!, mx: (colorRanges[color]?.1)!)) / 360.0)
+        hueValueLabel.text = "\(hue)"
+        
+        return UIColor(hue: hue, saturation: 0.66, brightness: 0.66, alpha: 1.0)
+    }
+    
+    func getRandomInt(mn: Int, mx: Int) -> Int {
+        return Int(arc4random_uniform(UInt32(mx - mn))) + mn
     }
     
     func getRandomColor() -> UIColor {
